ac227649ff1a12f3b47cf72c93670011f9d67276
avrilranezca <fernandezavril16@gmail.com>
2016-08-12 23:28:11 +0800

    Added ps.close() and rs.close() to all DAOs

--- src/database/AddressDAO.java
+++ src/database/AddressDAO.java
@@ -16,8 +16,11 @@ public class AddressDAO {
 	}
 	
 	public void addAddress(Address a) {
+		
+		PreparedStatement ps = null;
+		
 		try{
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO address (house_no, street, subdivision, city, postal_code, country) VALUES(?, ?, ?, ?, ?, ?);");
+			ps = conn.prepareStatement("INSERT INTO address (house_no, street, subdivision, city, postal_code, country) VALUES(?, ?, ?, ?, ?, ?);");
 			ps.setString(1, a.getHouse_no());
 			ps.setString(2, a.getStreet());
 			ps.setString(3, a.getSubdivision());
@@ -26,13 +29,22 @@ public class AddressDAO {
 			ps.setString(6, a.getCountry());
 			
 			ps.execute();
+			
 		}catch(SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	public int getAddress(Address a) {
-		PreparedStatement ps;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 
 		int id = -1;
 		try {
@@ -45,7 +57,7 @@ public class AddressDAO {
 			ps.setString(5, a.getPostal_code());
 			ps.setString(6, a.getCountry());
 			
-			ResultSet rs = ps.executeQuery();
+			rs = ps.executeQuery();
 			
 			
 			while(rs.next()) {
@@ -55,6 +67,19 @@ public class AddressDAO {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		return id;
 	}
